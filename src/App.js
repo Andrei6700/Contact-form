cf4571c85e86b7d52c3e9553e2448644a95e691e
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-04-16 21:39:57 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Form } from "./form/Form";
+
+function App() {
+
+    const user = async (event) =>{
+      event.preventDefault();
+      let formData = {
+        name: event.target[0].value ,
+        email: event.target[1].value ,
+        text: event.target[2].value ,
+      };
+    };
+
+  return (
+    <div className= "Aplicatie">
+          <p>Contact Us:</p>
+          <Form/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/form/Form.js
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+    export const Form = () => {
+        
+         const schema = yup.object().shape({
+            name: yup.string()
+                .max(64,'Maxim 64 de caractere').required('Formular invalid'),
+            email: yup.string()
+                .email('Invalid email').required('Formular invalid'),
+            message: yup.string()
+                .max(1024,'maxim 1024 de caractere').required('Formular invalid')   
+        });
+
+        const {register, handleSubmit, formState:{errors}} = useForm({
+            resolver: yupResolver(schema),
+        });
+        const onSubmit = (data) =>{
+            console.log(data);
+        };
+
+        
+
+        return(
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input className="name"  placeholder="Your name" {...register("name")}/>
+                <p className="errors">{errors.name?.message}</p>
+
+                    <input className="email" placeholder="Your email" {...register("email")}/>
+                    <p className="errors">{errors.email?.message}</p>
+
+                        <input className="text"  placeholder="Your message" {...register("message")}/>
+                        <p className="errors">{errors.message?.message}</p>
+            <div>
+                <label style={{ fontSize: '15px' }}>
+                    Mesaj Urgent<input className="check"  type="checkbox"/>
+                </label>
+                <input className="submit" type="submit" value="Trimite" />
+            </div>
+                
+        </form>
+             )
+}
