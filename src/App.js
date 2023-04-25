01eeff58771c5b3f638844120712fa677ef8c4be
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-04-25 10:07:08 +0300

    Update project files

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import { Form , bordercolor } from "./form/Form";
 
 function App() {
-    const user = async (event) =>{
-      event.preventDefault();
-      let formData = {
-        name: event.target[0].value ,
-        email: event.target[1].value ,
-        text: event.target[2].value ,
-      };
-    };
+  const user = async (event) =>{
+    event.preventDefault();
+    let formData = {
+    name: event.target[0].value ,
+    email: event.target[1].value ,
+    text: event.target[2].value ,};
+};
 
-    return (
-      <div className="Aplicatie">
-      <p>Contact Us:</p>
-      <Form />
-    </div>
-    );
+return (
+    <div className="body">
+        <div className="Aplicatie">
+        <p className="title">Contact Us:</p>
+        <Form/>
+        </div>
+    </div>);
 }
 
 export default App;
