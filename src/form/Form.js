76afe90cc2039cf2be00c763258ea0b5787b4f0c
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-05-15 19:54:04 +0300

    Fix useFormData usage

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,28 +1,13 @@
 
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import { useForm } from "react-hook-form";
-import React, { useState } from 'react';
+import React from 'react';
 import './Form-style.css';
 import { InputField } from "./components/input-field";
 import { InputButtons } from "./components/input-button";
 import { OnSubmit } from './components/on-submit';
+import { useFormData } from './components/form-data';
 
-
-export function Form() {
-  const schema = yup.object().shape({
-      name: yup.string().matches(/^[^\d]+$/, 'Invalid name')
-          .max(64, 'Maxim 64 characters').required('Invalid form'),
-      email: yup.string()
-          .email('Invalid email').required('Invalid form'),
-      message: yup.string().matches(/^[a-zA-Z\s]+$/, 'Invalid message')
-          .max(1024, 'Maxim 1024 characters').required('Invalid form'),
-      urgent: yup.boolean(),
-  });
-
-  const { register, handleSubmit, formState: { errors } } = useForm({
-      resolver: yupResolver(schema)
-  });
+export const Form = () => {
+  const { register, handleSubmit, errors } = useFormData();
 
   return (
     <form onSubmit={handleSubmit(OnSubmit)}>
@@ -31,6 +16,3 @@ export function Form() {
     </form>
   )
 }
-
-
-
