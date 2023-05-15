0c2c68feeaddfa4f3e902ecc8ff2105bd09dee68
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-05-15 19:59:40 +0300

    Format input-field.js

--- src/form/components/input-field.js
+++ src/form/components/input-field.js
@@ -1,26 +1,23 @@
 import React from "react";
 
- export function InputField({register,errors}) {
+export function InputField({ register, errors }) {
   const bordercolor = (input) => {
     if (errors[input])
       return 'red';
     else
       return 'white';
-   }
+  }
 
-    return(
-      <div>
-        <input className="name" placeholder="Your name" style={{ borderColor: bordercolor('name') }} {...register("name")} />
-        <p className="errors">{errors.name?.message}</p>
+  return (
+    <div>
+      <input className="name" placeholder="Your name" style={{ borderColor: bordercolor('name') }} {...register("name")} />
+      <p className="errors">{errors.name?.message}</p>
 
-        <input className="email" placeholder="Your email" style={{ borderColor: bordercolor('email') }} {...register("email")} />
+      <input className="email" placeholder="Your email" style={{ borderColor: bordercolor('email') }} {...register("email")} />
       <p className="errors">{errors.email?.message}</p>
 
       <input className="text" placeholder="Your message" style={{ borderColor: bordercolor('message') }}{...register("message")} />
       <p className="errors">{errors.message?.message}</p>
-        
-      </div>
-
-    )
-    
-  }
+    </div>
+  )
+}
