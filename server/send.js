549f7747cfa967c9e1a2ae2b180ffd4c34c6b906
Andrei6700 <91980199+Andrei6700@users.noreply.github.com>
2023-05-26 11:13:40 +0300

    Update send.js

--- server/send.js
+++ server/send.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
    service: "gmail",
    auth: {
       user: "andreibalanoiu67@gmail.com",
-      pass: "pfqxrgqwxjcmewgs"
+      pass: ""
    }
 });
 
